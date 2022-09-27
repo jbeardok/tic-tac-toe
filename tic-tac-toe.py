e70da5ed7523fe14d2882c8f784132c28ec18b63
James Beard <jbeardok@outlook.com>
2022-09-26 20:17:13 -0500

    Imports random, show_board() function is added, Adds choice to be X or O

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,3 +1,5 @@
+import random
+
 board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
 title = '''
 |///////////////////////////////////////////////////|
@@ -8,6 +10,22 @@ title = '''
 | #  ### ###      #  #   #  ###       #   ###   ### | 
 |///////////////////////////////////////////////////|
 '''
-print(title)
-for row in board:
-    print(row)
+def show_board():
+    print(title)
+    for row in board:
+        print(row)
+    print()
+
+human = input("Will you be X or O? ")
+human = human.upper()
+computer = ''
+print("You will be " + human)
+
+if human == 'X':
+    computer == 'O'
+    row = int(input("Enter the row: "))
+    column = int(input("Enter the cell (column): "))
+    board[row][column] = human
+else:
+    computer == 'X'
+show_board()
