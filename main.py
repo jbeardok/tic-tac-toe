c37dad56e09aba9579d0e764205bba837787443d
James Beard <jbeardok@outlook.com>
2023-03-30 01:31:46 -0500

    Add functions to organize and reduce

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from random import *
+
+def check_match(player_list):
+    # Checks rows, cols, forward and back diags
+    for player in player_list:
+        for i in range(3):
+            if board[i][0]==board[i][1]==board[i][2]==player or \
+        board[0][i]==board[1][i]==board[2][i]==player or \
+        board[0][0]==board[int(n/2)][int(n/2)]==board[n-1][n-1]==player or \
+        board[n-1][0]==board[int(n/2)][int(n/2)]==board[0][n-1]==player:
+                return True
+    return False
+
+def display_board():
+    print()
+    for r in board:
+        print(r)
+
 board = [['-'] * 3 for i in range(3)]
-for r in board:
-    print(r)
-# board = [['-', '-', '-'],
-#          ['-', '-', '-'],
-#          ['-', '-', '-']]
 n = len(board)
+
 match = False
 # Game is over once 9 turns have passed with no match
 for x in range(9):
+    display_board()
     # Begin round, player's turn
     player = True
     while player:
@@ -27,26 +41,10 @@ for x in range(9):
             if board[r][c] == '-':
                 board[r][c] = 'O' 
                 player = False
-    # Display board in terminal
-    print()
-    for r in board:
-        print(r)
-    # Check for a match
-    # rows, cols, diags
-    for i in range(3):
-        if board[i][0]==board[i][1]==board[i][2]=='X' or \
-        board[0][i]==board[1][i]==board[2][i]=='X' or \
-        board[0][0]==board[int(n/2)][int(n/2)]==board[n-1][n-1]=='X' or \
-        board[n-1][0]==board[int(n/2)][int(n/2)]==board[0][n-1]=='X':
-            # if match is found, break game loop
-            match = True
-        if board[i][0]==board[i][1]==board[i][2]=='O' or \
-        board[0][i]==board[1][i]==board[2][i]=='O' or \
-        board[0][0]==board[int(n/2)][int(n/2)]==board[n-1][n-1]=='O' or \
-        board[n-1][0]==board[int(n/2)][int(n/2)]==board[0][n-1]=='O':
-            match = True
+
+    match = check_match(['X', 'O'])
     if match:
         print('win')
         break
 if not match:
-    print('board full')               
+    print('board is full')               
